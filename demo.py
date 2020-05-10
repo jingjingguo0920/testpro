1d5262c3d2946ddd9a7606d21b7de22e0031a593
jingjing.guo <jingjing.guo@bytedance.com>
2020-05-10 10:42:51 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,17 @@
+# from python.demo1 import Demo1
+#tips option+回车,导入模块
+# from testing import demo2
+
+# from testing.demo2 import Demo2
+# from testing.demo2 import *
+#
+# # Demo1()
+# # Demo2()
+#
+# # print(demo2.hello)
+# # demo2.f()
+# print(hello)
+# f()
+
+from testing import *
+
---
+++ testing/demo2.py
@@ -0,0 +1,10 @@
+# _all_提供了一个对外暴露的可以使用的方法/变量的列表
+__all__ = ['hello','f']
+
+hello='hello ,demo2'
+
+def f():
+    print("demo2.py f()")
+
+class Demo2:
+    pass
